9b531fafc6a772c044a15fa16d13d885e5ecc638
Henrique Couto <henrique.coutu@live.com>
2020-08-16 13:51:22 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -16,10 +16,17 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid@email.com')
     expect(isEmailValid).toBe(true)
   })
+
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const { sut } = makeSut()
     const isEmailValid = sut.isValid('invalid@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const { sut } = makeSut()
+    sut.isValid('any@email.com')
+    expect(validator.email).toBe('any@email.com')
   })
 })
