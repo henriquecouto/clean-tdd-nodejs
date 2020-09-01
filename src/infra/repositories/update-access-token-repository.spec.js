1ff7813ab3d167ffd3ae933bb387175405e91f95
Henrique Couto <henrique.coutu@live.com>
2020-09-01 10:35:02 -0300

    ensure UpdateAccessTokenRepository updates the user with the given accessToken

---
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -0,0 +1,36 @@
+const MongoHelper = require('../helpers/mongo-helper')
+let db
+
+class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, accessToken) {
+    await this.userModel.updateOne({ _id: userId }, { $set: { accessToken } })
+  }
+}
+
+describe('UpdateAccessToken Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.getDB()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should update the user with the given accessToken', async () => {
+    const userModel = db.collection('users')
+    const fakeUser = await userModel.insertOne({ email: 'valid@mail.com' })
+    const sut = new UpdateAccessTokenRepository(userModel)
+    await sut.update(fakeUser.ops[0]._id, 'valid_token')
+    const updatedFakeUser = await userModel.findOne({ _id: fakeUser.ops[0]._id })
+    expect(updatedFakeUser.accessToken).toBe('valid_token')
+  })
+})
