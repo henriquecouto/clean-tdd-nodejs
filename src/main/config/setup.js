5cc0e3f952c8308f2a0663bf659754291f6e27b5
Henrique Couto <henrique.coutu@live.com>
2020-09-04 21:49:56 -0300

    ensure requests returns json content type as default

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,10 @@
 const cors = require('../middlewares/cors')
 const json = require('../middlewares/json-parser')
+const contentType = require('../middlewares/content-type')
 
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(json)
+  app.use(contentType)
 }
