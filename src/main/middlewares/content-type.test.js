fc3216910f16fdd5aff797d9ef4ed5f733e78ba6
Henrique Couto <henrique.coutu@live.com>
2020-09-04 22:03:31 -0300

    ensure requests returns given content type if set up

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type Middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('Should return json content type as default', async () => {
     app.get('/test-content-type', (req, res) => {
       res.send('')
@@ -9,5 +15,15 @@ describe('Content-Type Middleware', () => {
     await request(app)
       .get('/test-content-type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type if set up', async () => {
+    app.get('/test-content-type', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test-content-type')
+      .expect('content-type', /xml/)
   })
 })
