d242485edd845e3b09c1d46727069940d7ab3ebb
Henrique Couto <henrique.coutu@live.com>
2020-09-06 18:28:20 -0300

    ensure LoginRoutes returns 200 response when valid credentials are provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,3 +1,13 @@
+jest.mock('jsonwebtoken', () => ({
+  token: 'any_token',
+
+  sign (payload, secret) {
+    this.payload = payload
+    this.secret = secret
+    return this.token
+  }
+}))
+
 const jwt = require('jsonwebtoken')
 const MissingParamError = require('../errors/missing-param-error')
 const TokenGenerator = require('./token-generator')
@@ -24,7 +34,7 @@ describe('Token Generator', () => {
   test('Should return a token if JWT returns a token', async () => {
     const { sut } = makeSut()
     await sut.generate('any_id')
-    expect(jwt.id).toBe('any_id')
+    expect(jwt.payload).toEqual({ _id: 'any_id' })
     expect(jwt.secret).toBe(sut.secret)
   })
 
