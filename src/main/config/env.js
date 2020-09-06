26621246a6cc76c32174d083495956921a77001e
Henrique Couto <henrique.coutu@live.com>
2020-09-06 11:57:42 -0300

    login router composition

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-tdd-js'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-tdd-js',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
